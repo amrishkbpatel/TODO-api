dfe42cefec283adfb6b6ea9b7e25060fd7348836
Amrish patel <amrish.patel@parc.com>
2016-09-19 14:41:53 -0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app =express();
 var PORT = process.env.PORT || 3000;
@@ -22,13 +23,8 @@ app.get('/todos', function(req, res){
 //GET todos/id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if(todoId === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
 	if(matchedTodo){
 		res.json(matchedTodo);
 	} else {
@@ -39,8 +35,14 @@ app.get('/todos/:id', function(req, res){
 
 //POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, "description", "completed");
 
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(404).send();
+	}
+
+
+	body.description = body.description.trim();
 	//add id field
 	body.id = todoNextId++;
 
